ac4a8ee8f62e5f9563a8d27c8f324c1a89ec8a04
Robert Wendt <robert.wendt@yandex.ru>
2026-04-01 23:17:05 +0300

    Output information in motor

--- football2/motor.cpp
+++ football2/motor.cpp
@@ -1,12 +1,26 @@
+#include "Arduino.h"
 #include "motor.h"
 
 Motor::Motor(int location_angle, int pin_1, int pin_2, int direction, int angle_coef) : _location_angle{location_angle}, _pin_1{pin_1}, _pin_2{pin_2}, _direction{direction}, _angle_coef{angle_coef}, _rotation_speed{} {}
 
 void Motor::set_velocity(float linear_speed, float angle, float angular_speed)
 {
-  _rotation_speed = _direction * (linear_speed * cosf(_location_angle + _angle_coef * angle) + angular_speed);
-  // _rotation_speed = _direction * (linear_speed * (cosf(_location_angle + _angle_coef * angle) + angular_speed)); // Это второй вариант, я не знаю пока какой нужен
-  //------------------------------------------------^-----------------------------------------------------------^--
+  _rotation_speed = _direction * (linear_speed * cosf(_location_angle * DEG_TO_RAD + _angle_coef * angle * DEG_TO_RAD) + angular_speed);
+
+  Serial.print(_rotation_speed);
+  Serial.print(" = ");
+  Serial.print((_direction));
+  Serial.print(" * ");
+  Serial.print(" (");
+  Serial.print(linear_speed);
+  Serial.print(" * ");
+  Serial.print("cos(");
+  Serial.print(_location_angle);
+  Serial.print(" + ");
+  Serial.print(angle);
+  Serial.print(" ) + ");
+  Serial.print(angular_speed);
+  Serial.println(" )");
 }
 
 void Motor::run()
@@ -37,5 +51,5 @@ void Motor::debug()
   Serial.print("\tSpeed:\t");
   Serial.println(_rotation_speed);
   Serial.println("==============================================");
-  delay(1000);
+  delay(100);
 }
